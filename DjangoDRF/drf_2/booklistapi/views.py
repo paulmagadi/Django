1b59496f5f9f0f9fc0fa7ccb500cd146c11e69d0
SQCodes <paulsqmagadi@gmail.com>
2024-03-18 13:57:24 +0300

    modified:   DjangoDRF/drf_2/booklistapi/views.py

--- DjangoDRF/drf_2/booklistapi/views.py
+++ DjangoDRF/drf_2/booklistapi/views.py
@@ -3,8 +3,16 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from django.http import HttpResponse
 
 # Create your views here.
-@api_view(['POST'])
+# @api_view(['POST'])
+# def books(request):
+#     return Response('list of books', status=status.HTTP_200_OK)
+
+def books(request):
+    return HttpResponse('list of books', status=status.HTTP_200_OK)
+
+@api_view()
 def books(request):
     return Response('list of books', status=status.HTTP_200_OK)
