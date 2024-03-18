9bcbff704d0f623736ba4c53631189110a02186a
SQCodes <paulsqmagadi@gmail.com>
2024-03-18 13:42:34 +0300

    modified:   DjangoDRF/drf_2/Pipfile 	modified:   DjangoDRF/drf_2/Pipfile.lock 	modified:   DjangoDRF/drf_2/booklist/settings.py 	modified:   DjangoDRF/drf_2/booklist/urls.py

--- DjangoDRF/drf_2/booklist/settings.py
+++ DjangoDRF/drf_2/booklist/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+ddnhmlgiedh4i@q6&m$xe9ghg(^n7id6zrl9@&foiau(*#!#@'
+SECRET_KEY = 'django-insecure-m+t#e)yj*^w)a0x27i49grh5(=t8oaf-vz4&^dbt^j0mit@(_v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'booklistapi',
 ]
 
 MIDDLEWARE = [
