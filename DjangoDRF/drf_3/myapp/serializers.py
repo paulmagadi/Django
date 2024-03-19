0225f30032f8eee5ae33557d6ea6ba3453465419
SQCodes <paulsqmagadi@gmail.com>
2024-03-19 16:48:37 +0300

    modified:   DjangoDRF/drf_3/myapp/serializers.py

--- DjangoDRF/drf_3/myapp/serializers.py
+++ DjangoDRF/drf_3/myapp/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import MenuItem
+from decimal import Decimal
 
 
 
@@ -13,9 +14,31 @@ from .models import MenuItem
 
 
 # MODEL SERIALIZERS
+# class MenuItemSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = MenuItem
+#         fields = ['id', 'title', 'price', 'inventory']
+    
+  
+  #CREATE A NEW FIELD
+# class MenuItemSerializer(serializers.ModelSerializer):
+#   stock = serializers.IntegerField(source='inventory')
+#   class Meta:
+#     model = MenuItem
+#     fields = ['id', 'title', 'price', 'stock']
+  
+  
+  
+  
 class MenuItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MenuItem
-        fields = ['id', 'title', 'price', 'inventory']
+  stock = serializers.IntegerField(source='inventory')
+  price_after_tax = serializers.SerializerMethodField(method_name= 'calculate_tax')
+  class Meta:
+      model = MenuItem
+      fields = ['id', 'title', 'price', 'stock', 'price_after_tax']  
+      
+  def calculate_tax(self, product:MenuItem):
+    return product.price * Decimal(1.1)
+      
     
-    
+      
