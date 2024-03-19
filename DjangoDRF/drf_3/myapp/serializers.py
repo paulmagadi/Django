9d82e5b9599d435491d5e2db8985ebef2efc9ea5
SQCodes <paulsqmagadi@gmail.com>
2024-03-19 16:39:01 +0300

    modified:   DjangoDRF/drf_3/myapp/serializers.py

--- DjangoDRF/drf_3/myapp/serializers.py
+++ DjangoDRF/drf_3/myapp/serializers.py
@@ -1,29 +1,21 @@
 from rest_framework import serializers
 from .models import MenuItem
 
-# class ArticleSerializer(serializers.Serializer):
-#     title = serializers.CharField(max_length=100)
-#     author = serializers.CharField(max_length=100)
-#     email = serializers.EmailField(max_length=255)
-#     date = serializers.DateTimeField()
-    
-#     def create(self, validated_data):
-#         return Article.object.create(validated_data)
-    
-#     def update(self, instance, validated_data):
-#         instance.title = validated_data.get('title', instance.title)
-#         instance.author = validated_data.get('author', instance.author)
-#         instance.email = validated_data.get('email', instance.email)
-#         instance.date = validated_data.get('date', instance.date)
-#         instance.save()
-#         return instance
 
 
-class MenuItemSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=8, decimal_places=2)
-    inventory = serializers.IntegerField()
+
+# class MenuItemSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
+#     price = serializers.DecimalField(max_digits=8, decimal_places=2)
+#     inventory = serializers.IntegerField()
     
 
+
+# MODEL SERIALIZERS
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuItem
+        fields = ['id', 'title', 'price', 'inventory']
+    
     
