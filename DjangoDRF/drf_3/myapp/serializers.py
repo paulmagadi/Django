6ca7ae3dc05c00cf11b1380758c0158104637126
SQCodes <paulsqmagadi@gmail.com>
2024-03-19 16:18:45 +0300

    modified:   DjangoDRF/drf_3/db.sqlite3 	modified:   DjangoDRF/drf_3/myapp/serializers.py 	modified:   DjangoDRF/drf_3/myapp/views.py

--- DjangoDRF/drf_3/myapp/serializers.py
+++ DjangoDRF/drf_3/myapp/serializers.py
@@ -1,22 +1,29 @@
 from rest_framework import serializers
-from .models import Article
+from .models import MenuItem
 
-class ArticleSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=100)
-    author = serializers.CharField(max_length=100)
-    email = serializers.EmailField(max_length=255)
-    date = serializers.DateTimeField()
+# class ArticleSerializer(serializers.Serializer):
+#     title = serializers.CharField(max_length=100)
+#     author = serializers.CharField(max_length=100)
+#     email = serializers.EmailField(max_length=255)
+#     date = serializers.DateTimeField()
     
-    def create(self, validated_data):
-        return Article.object.create(validated_data)
+#     def create(self, validated_data):
+#         return Article.object.create(validated_data)
     
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.author = validated_data.get('author', instance.author)
-        instance.email = validated_data.get('email', instance.email)
-        instance.date = validated_data.get('date', instance.date)
-        instance.save()
-        return instance
+#     def update(self, instance, validated_data):
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.author = validated_data.get('author', instance.author)
+#         instance.email = validated_data.get('email', instance.email)
+#         instance.date = validated_data.get('date', instance.date)
+#         instance.save()
+#         return instance
+
+
+class MenuItemSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+    price = serializers.DecimalField(max_digits=8, decimal_places=2)
+    inventory = serializers.IntegerField()
     
 
     
--- DjangoDRF/drf_3/myapp/views.py
+++ DjangoDRF/drf_3/myapp/views.py
@@ -3,10 +3,16 @@ from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import MenuItem
+from .serializers import MenuItemSerializer
 
 # Create your views here.
+# @api_view()
+# def menu_items(request):
+#     items = MenuItem.objects.all()
+#     return Response(items.values())
+
 @api_view()
 def menu_items(request):
     items = MenuItem.objects.all()
-    return Response(items.values())
-
+    serialized_items = MenuItemSerializer(items, many=True)
+    return Response(serialized_items.data)
