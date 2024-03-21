8c31821e3a883db4b306e10d245efad4a1b08fee
SQCodes <paulsqmagadi@gmail.com>
2024-03-21 16:34:21 +0300

    modified:   DjangoDRF/drf_3/myproject/settings.py

--- DjangoDRF/drf_3/myproject/settings.py
+++ DjangoDRF/drf_3/myproject/settings.py
@@ -130,10 +130,12 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
-        # 'rest_framework_xml.renderers.XMLRenderer'
+        # 'rest_framework_xml.renderers.XMLRenderer',
+        
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_THROTTLE_RATES': {
         'anon':'2/minute',
