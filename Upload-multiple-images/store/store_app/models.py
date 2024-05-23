db34816a0f1c8bcc59c693a0cd3477960afc6c60
SQCodes <paulsqmagadi@gmail.com>
2024-05-23 23:29:51 +0300

    modified:   Upload-multiple-images/store/store_app/models.py

--- Upload-multiple-images/store/store_app/models.py
+++ Upload-multiple-images/store/store_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    images = models.ImageField(upload_to='products/', blank=True, null=True)
