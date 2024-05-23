be4d6fe7387867d7bd9dc115e0bed28a4d689d78
SQCodes <paulsqmagadi@gmail.com>
2024-05-24 00:39:45 +0300

    modified:   Upload-multiple-images/store/db.sqlite3 	modified:   Upload-multiple-images/store/store_app/__pycache__/models.cpython-311.pyc 	new file:   Upload-multiple-images/store/store_app/migrations/0002_uploadedfile_title.py 	new file:   Upload-multiple-images/store/store_app/migrations/__pycache__/0002_uploadedfile_title.cpython-311.pyc 	modified:   Upload-multiple-images/store/store_app/models.py

--- Upload-multiple-images/store/store_app/models.py
+++ Upload-multiple-images/store/store_app/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    price =  models.DecimalField(max_digits=12, decimal_places=2)
+
 class UploadedFile(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     file = models.FileField(upload_to='uploads/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
+    
+    
