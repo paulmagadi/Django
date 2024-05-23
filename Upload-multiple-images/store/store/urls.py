0934393cf91ce2d5ebce3f817b7822e47ecea29e
SQCodes <paulsqmagadi@gmail.com>
2024-05-23 23:45:42 +0300

    modified:   Upload-multiple-images/store/store/urls.py

--- Upload-multiple-images/store/store/urls.py
+++ Upload-multiple-images/store/store/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,8 +29,3 @@ urlpatterns = [
 
 
 
-
-from django.conf import settings
-from django.conf.urls.static import static
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, 
