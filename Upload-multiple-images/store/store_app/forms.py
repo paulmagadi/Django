6371a50da8e2153679f4f3c876801a302b66f66b
SQCodes <paulsqmagadi@gmail.com>
2024-05-23 23:40:25 +0300

    modified:   Upload-multiple-images/store/store_app/forms.py

--- Upload-multiple-images/store/store_app/forms.py
+++ Upload-multiple-images/store/store_app/forms.py
@@ -1,12 +1,20 @@
 from django import forms
-from django.forms import ModelForm
-from django.forms.fields import MultipleChoiceField
 
-from store_app.models import Product
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
 
-class ProductForm(ModelForm):
-    images = MultipleChoiceField(widget=forms.FileInput(attrs={'multiple': True}))
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
 
-    class Meta:
-        model = Product
-        fields = ['name', 'description', 'images']
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = single_file_clean(data, initial)
+        return result
+
+class UploadFileForm(forms.Form):
+    files = MultipleFileField()
