5a5b7f661988d62d64141349cb873a0d062eeb8a
SQCodes <paulsqmagadi@gmail.com>
2024-05-23 23:54:52 +0300

    modified:   Upload-multiple-images/store/store/__pycache__/settings.cpython-311.pyc 	modified:   Upload-multiple-images/store/store/__pycache__/urls.cpython-311.pyc 	modified:   Upload-multiple-images/store/store/settings.py 	modified:   Upload-multiple-images/store/store/urls.py

--- Upload-multiple-images/store/store/settings.py
+++ Upload-multiple-images/store/store/settings.py
@@ -118,13 +118,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR /'static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR /'static'
+# ]
 
-MEDIA_URL = 'media/'
-
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
