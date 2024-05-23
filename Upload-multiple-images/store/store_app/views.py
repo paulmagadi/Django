6f856498b9975671dbbedeaab14a8a315bd566b2
SQCodes <paulsqmagadi@gmail.com>
2024-05-23 23:33:51 +0300

    modified:   Upload-multiple-images/store/store_app/views.py

--- Upload-multiple-images/store/store_app/views.py
+++ Upload-multiple-images/store/store_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import CreateView
+
+from store_app.forms import ProductForm
+from store_app.models import Product
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'product_create.html'
