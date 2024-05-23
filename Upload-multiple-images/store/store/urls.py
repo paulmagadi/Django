27ebf9de7be4189497c30266ddee311858b9ed3f
SQCodes <paulsqmagadi@gmail.com>
2024-05-23 23:45:09 +0300

    modified:   Upload-multiple-images/store/store/urls.py

--- Upload-multiple-images/store/store/urls.py
+++ Upload-multiple-images/store/store/urls.py
@@ -15,18 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('fileuploads.urls')),
+    path('', include('store.urls')),
 ]
+
+
+
+
 
 from django.conf import settings
 from django.conf.urls.static import static
