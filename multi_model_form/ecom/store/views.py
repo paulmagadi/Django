6b4307b97c2130583a2bebae8ca9497c69957f57
SQCodes <paulsqmagadi@gmail.com>
2024-05-24 16:15:46 +0300

    modified:   multi_model_form/ecom/store/__pycache__/forms.cpython-311.pyc 	modified:   multi_model_form/ecom/store/__pycache__/views.cpython-311.pyc 	modified:   multi_model_form/ecom/store/views.py

--- multi_model_form/ecom/store/views.py
+++ multi_model_form/ecom/store/views.py
@@ -1,51 +1,43 @@
 from django.views.generic.base import TemplateView
-from .models import Person, Bio, Email
-from .forms import PersonForm, BioForm, EmailForm
-from django.shortcuts import get_object_or_404
+from .models import Product, ProductImage
+from .forms import ProductForm, ProductImageForm
+from django.shortcuts import get_object_or_404, redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import messages
 
-class PersonDetailUpdateView(TemplateView):
-    template_name = 'home.html'
+class ProductDetailUpdateView(TemplateView):
+    template_name = 'shop/product_detail_update_form.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        person = get_object_or_404(Person, pk=self.kwargs['pk'])
-        context['person'] = person
-        context['person_form'] = PersonForm(instance=person)
-        context['bio_form'] = BioForm(instance=person.bio)
-        context['email_form'] = EmailForm(instance=person.email)
+        product = get_object_or_404(Product, pk=self.kwargs['pk'])
+        context['product'] = product
+        context['product_form'] = ProductForm(instance=product)
+        context['product_image_form'] = ProductImageForm()
+        context['product_images'] = ProductImage.objects.filter(product=product)
         return context
     
     def post(self, request, *args, **kwargs):
         pk = self.kwargs['pk']
-        person = get_object_or_404(Person, pk=pk)
-        person_form = PersonForm(instance=person, data=request.POST)
-        bio = get_object_or_404(Bio, person=person)
-        bio_form = BioForm(instance=bio, data=request.POST)
-        email = get_object_or_404(Email, person=person)
-        email_form = EmailForm(instance=email, data=request.POST)
+        product = get_object_or_404(Product, pk=pk)
+        product_form = ProductForm(instance=product, data=request.POST)
+        product_image_form = ProductImageForm(request.POST, request.FILES)
         
-        if 'save_person' in request.POST:
-            if person_form.is_bound and person_form.is_valid():
-                person_form.save()
-                messages.add_message(request, messages.SUCCESS, f"Person details updated successfully.")
+        if 'save_product' in request.POST:
+            if product_form.is_bound and product_form.is_valid():
+                product_form.save()
+                messages.add_message(request, messages.SUCCESS, "Product details updated successfully.")
             else:
-                messages.error(request, person_form.errors)
+                messages.error(request, product_form.errors)
         
-        elif 'save_bio' in request.POST:
-            if bio_form.is_bound and bio_form.is_valid():
-                bio_form.save()
-                messages.add_message(request, messages.SUCCESS, f"Bio updated successfully.")
+        elif 'save_product_image' in request.POST:
+            if product_image_form.is_bound and product_image_form.is_valid():
+                product_image = product_image_form.save(commit=False)
+                product_image.product = product
+                product_image.save()
+                messages.add_message(request, messages.SUCCESS, "Product image added successfully.")
             else:
-                messages.error(request, bio_form.errors)
+                messages.error(request, product_image_form.errors)
         
-        elif 'save_email' in request.POST:
-            if email_form.is_bound and email_form.is_valid():
-                email_form.save()
-                messages.add_message(request, messages.SUCCESS, f"Email updated successfully.")
-            else:
-                messages.error(request, email_form.errors)
-        
-        return HttpResponseRedirect(reverse('home', kwargs={'pk': pk}))
+        return HttpResponseRedirect(reverse('shop:product_detail_update_form', kwargs={'pk': pk}))
