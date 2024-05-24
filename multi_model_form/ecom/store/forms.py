5367010b948ff710959b649b1fdc1239b506a88a
SQCodes <paulsqmagadi@gmail.com>
2024-05-24 18:30:27 +0300

    modified:   multi_model_form/ecom/ecom/__pycache__/settings.cpython-311.pyc 	modified:   multi_model_form/ecom/ecom/settings.py 	modified:   multi_model_form/ecom/store/__pycache__/forms.cpython-311.pyc 	modified:   multi_model_form/ecom/store/forms.py

--- multi_model_form/ecom/store/forms.py
+++ multi_model_form/ecom/store/forms.py
@@ -1,29 +1,45 @@
 from django import forms
 from django.forms import ModelForm
 from .models import Product, ProductImage
-from multifilefield.fields import MultiFileField
+
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = [single_file_clean(data, initial)]
+        return result
 
 class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'price', 'description']
 
-# class ProductImageForm(ModelForm):
-#     class Meta:
-#         model = ProductImage
-#         fields = ['image']
-#         widgets = {'image': forms.FileInput(attrs={'multiple': True})}
+class ProductImageForm(ModelForm):
+    class Meta:
+        model = ProductImage
+        fields = ['image']
+        widgets = {'image': forms.FileField()}
         
 
 
 
-class ProductImageForm(forms.ModelForm):
-    images = MultiFileField(
-        min_num=1,  # minimum number of files required
-        max_num=10,  # maximum number of files allowed
-        max_file_size=1024*1024*5,  # 5 MB
-        required=True
-    )
+# class ProductImageForm(forms.ModelForm):
+#     images = MultiFileField(
+#         min_num=1,  # minimum number of files required
+#         max_num=10,  # maximum number of files allowed
+#         max_file_size=1024*1024*5,  # 5 MB
+#         required=True
+    # )
 
     class Meta:
         model = ProductImage
