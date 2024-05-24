4b2222e63a86c6b381f88b9afce25324f918d536
SQCodes <paulsqmagadi@gmail.com>
2024-05-24 14:30:31 +0300

    modified:   Upload-multiple-images/ecom/db.sqlite3 	new file:   Upload-multiple-images/ecom/media/products/footwear-5408643_1280.jpg 	new file:   Upload-multiple-images/ecom/media/products/leather-shoe-3706748_1280.jpg 	new file:   Upload-multiple-images/ecom/media/products/running-shoe-321199_1280.jpg 	new file:   Upload-multiple-images/ecom/media/products/running-shoe-371624_1280.jpg 	modified:   Upload-multiple-images/ecom/store/__pycache__/admin.cpython-311.pyc 	modified:   Upload-multiple-images/ecom/store/__pycache__/models.cpython-311.pyc 	modified:   Upload-multiple-images/ecom/store/admin.py 	new file:   Upload-multiple-images/ecom/store/migrations/0002_remove_uploadedfile_product_delete_product.py 	new file:   Upload-multiple-images/ecom/store/migrations/__pycache__/0002_remove_uploadedfile_product_delete_product.cpython-311.pyc 	modified:   Upload-multiple-images/ecom/store/models.py

--- Upload-multiple-images/ecom/store/admin.py
+++ Upload-multiple-images/ecom/store/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import UploadedFile, Product
+from .models import UploadedFile
 
 # Register your models here.
-admin.site.register(Product)
 admin.site.register(UploadedFile)
--- Upload-multiple-images/ecom/store/models.py
+++ Upload-multiple-images/ecom/store/models.py
@@ -2,10 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Product(models.Model):
-    title = models.CharField(max_length=255)
-    price =  models.DecimalField(max_digits=12, decimal_places=2)
 
 class UploadedFile(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
     file = models.FileField(upload_to='products/')
