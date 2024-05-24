41b6ab3c01ce5eeec06d4115fa0edf5d061c190e
SQCodes <paulsqmagadi@gmail.com>
2024-05-24 12:26:18 +0300

    modified:   Upload-multiple-images/ecom/store/__pycache__/views.cpython-311.pyc 	new file:   Upload-multiple-images/ecom/store/forms.py 	modified:   Upload-multiple-images/ecom/store/views.py

--- Upload-multiple-images/ecom/store/views.py
+++ Upload-multiple-images/ecom/store/views.py
@@ -4,3 +4,21 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+from django.shortcuts import render, redirect
+from .models import UploadedFile
+from .forms import UploadFileForm
+
+def product(request):
+    files = UploadedFile.objects.all()
+
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            for uploaded_file in request.FILES.getlist('files'):
+                UploadedFile.objects.create(file=uploaded_file)
+            return redirect('product')
+    else:
+        form = UploadFileForm()
+
+    return render(request, 'upload_and_display.html', {'form': form, 'files': files})
