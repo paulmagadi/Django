a6e900d3b25a754078dc0f5894e5536e0c991e95
SQCodes <paulsqmagadi@gmail.com>
2024-06-01 14:15:21 +0300

    modified:   django-mptt/ecommerce/db.sqlite3 	modified:   django-mptt/ecommerce/store/__pycache__/admin.cpython-311.pyc 	modified:   django-mptt/ecommerce/store/__pycache__/models.cpython-311.pyc 	modified:   django-mptt/ecommerce/store/admin.py 	modified:   django-mptt/ecommerce/store/migrations/0001_initial.py 	modified:   django-mptt/ecommerce/store/migrations/__pycache__/0001_initial.cpython-311.pyc 	new file:   django-mptt/ecommerce/store/migrations/__pycache__/0002_specification_alter_category_options_and_more.cpython-311.pyc 	new file:   django-mptt/ecommerce/store/migrations/__pycache__/0003_alter_product_specification.cpython-311.pyc 	new file:   django-mptt/ecommerce/store/migrations/__pycache__/0004_alter_product_specification.cpython-311.pyc 	new file:   django-mptt/ecommerce/store/migrations/__pycache__/0005_remove_product_specification.cpython-311.pyc 	new file:   django-mptt/ecommerce/store/migrations/__pycache__/0006_product_specification.cpython-311.pyc 	new file:   django-mptt/ecommerce/store/migrations/__pycache__/0007_specificationtitle_specificationvalue_and_more.cpython-311.pyc 	new file:   django-mptt/ecommerce/store/migrations/__pycache__/0008_rename_value_specificationtitle_title_and_more.cpython-311.pyc 	new file:   django-mptt/ecommerce/store/migrations/__pycache__/0009_rename_value_specificationvalue_value.cpython-311.pyc 	new file:   django-mptt/ecommerce/store/migrations/__pycache__/0010_specificationtitle_level_specificationtitle_lft_and_more.cpython-311.pyc 	new file:   django-mptt/ecommerce/store/migrations/__pycache__/0011_rename_specificationtitle_specification_and_more.cpython-311.pyc 	new file:   django-mptt/ecommerce/store/migrations/__pycache__/0012_alter_product_specification.cpython-311.pyc 	new file:   django-mptt/ecommerce/store/migrations/__pycache__/0013_alter_product_specification.cpython-311.pyc 	modified:   django-mptt/ecommerce/store/models.py

--- django-mptt/ecommerce/store/admin.py
+++ django-mptt/ecommerce/store/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from mptt.admin import MPTTModelAdmin
-from .models import Category, Product
+from .models import Category, Product, Specification
 
 class CategoryAdmin(MPTTModelAdmin):
     list_display = ('name', 'parent')
@@ -8,10 +8,16 @@ class CategoryAdmin(MPTTModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 
+class SpecificationAdmin(MPTTModelAdmin):
+    list_display = ('title', 'parent')
+    search_fields = ('title',)
+    
+admin.site.register(Specification, SpecificationAdmin)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'category')
     search_fields = ('name', 'category__name')
     list_filter = ('category',)
 
 admin.site.register(Product, ProductAdmin)
+
 
--- django-mptt/ecommerce/store/models.py
+++ django-mptt/ecommerce/store/models.py
@@ -17,18 +17,19 @@ class Category(MPTTModel):
     def __str__(self):
         return self.name
     
-class Specification(models.Model):
+class Specification(MPTTModel):
     title = models.CharField(max_length=100, unique=True)
-    Value = models.CharField(max_length=100, unique=True)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
     
     def __str__(self):
         return self.title
+    
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=12, decimal_places=2)
     category = TreeForeignKey(Category, on_delete=models.CASCADE, related_name='products')
-    Specification= models.ForeignKey(Specification, on_delete=models.CASCADE)
+    Specification= TreeForeignKey(Specification, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
