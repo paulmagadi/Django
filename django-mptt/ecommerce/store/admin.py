14a19938e920755d74f305647e6e114ef6cd1f1b
SQCodes <paulsqmagadi@gmail.com>
2024-06-01 10:54:51 +0300

    modified:   django-mptt/ecommerce/db.sqlite3 	modified:   django-mptt/ecommerce/store/__pycache__/admin.cpython-311.pyc 	modified:   django-mptt/ecommerce/store/admin.py 	new file:   django-mptt/ecommerce/store/migrations/0001_initial.py 	new file:   django-mptt/ecommerce/store/migrations/__pycache__/0001_initial.cpython-311.pyc

--- django-mptt/ecommerce/store/admin.py
+++ django-mptt/ecommerce/store/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+from .models import Category, Product
 
-# Register your models here.
+class CategoryAdmin(MPTTModelAdmin):
+    list_display = ('name', 'parent')
+    search_fields = ('name',)
+
+admin.site.register(Category, CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
+    search_fields = ('name', 'category__name')
+    list_filter = ('category',)
+
+admin.site.register(Product, ProductAdmin)
+
