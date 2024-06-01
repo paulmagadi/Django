cf522340e1b11609b86440d95c584a2065eff0bc
SQCodes <paulsqmagadi@gmail.com>
2024-06-01 10:57:08 +0300

    new file:   django-mptt/ecommerce/store/__pycache__/forms.cpython-311.pyc 	modified:   django-mptt/ecommerce/store/__pycache__/views.cpython-311.pyc 	new file:   django-mptt/ecommerce/store/forms.py 	modified:   django-mptt/ecommerce/store/views.py

--- django-mptt/ecommerce/store/views.py
+++ django-mptt/ecommerce/store/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Product
+from .forms import ProductForm
 
 # Create your views here.
 def home(request):
     return render(request, 'store/index.html')
+
+def create_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')  
+    else:
+        form = ProductForm()
+    return render(request, 'create_product.html', {'form': form})
+
+def edit_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('home')  
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'edit_product.html', {'form': form, 'product': product})
